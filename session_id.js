af15a25d33d4e8a35d5c1e7380e749cb94759070
Paulwq12 <gracetacha1@gmail.com>
2024-12-21 16:45:08 +0100

    Update session_id.js

--- session_id.js
+++ session_id.js
@@ -43,5 +43,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&LE0S3QKT#EJl17DYP7G-BXAIl5_yQEAREGE2KFOkeArc_iDumDHw",
 };
